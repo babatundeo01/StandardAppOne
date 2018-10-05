44878991e26a49ee1d457f8e0fb047e04b73c641
babatundeo01 <babatunde.ojo@mobileappscompany.us>
2018-10-05 01:46:14 -0500

    Initial commit

---
+++ app/src/main/java/com/example/consultants/standardappone/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.consultants.standardappone;
+
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private Button launch;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        launch = findViewById(R.id.launchBTN);
+        launch.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View itemView) {
+        switch (itemView.getId()){
+            case R.id.launchBTN:
+                //here we process  action relevant to id
+                processIntToBeSent();
+                break;
+        }
+    }
+
+    private void processIntToBeSent() {
+        Intent sendInt = getPackageManager().getLaunchIntentForPackage("com.example.consultants.standardapptwo");
+        if (sendInt != null) {
+            startActivity(sendInt);
+        }
+    }
+}
